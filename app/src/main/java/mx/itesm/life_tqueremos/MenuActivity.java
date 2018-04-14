307179840ab1df21f2d6cd2a89104bb124bacb13
Iván Muñiz <ivanmr_96@live.com.mx>
2018-04-14 14:12:39 -0500

    adding navigation drawer without functionality

--- app/src/main/java/mx/itesm/life_tqueremos/MenuActivity.java
+++ app/src/main/java/mx/itesm/life_tqueremos/MenuActivity.java
@@ -2,8 +2,11 @@ package mx.itesm.life_tqueremos;
 
 import android.app.ListActivity;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -14,17 +17,35 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
-public class MenuActivity extends ListActivity {
+public class MenuActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        mDrawerLayout = findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        PollDimension pollDimension = new PollDimension("what", false);
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference mRef = firebaseDatabase.getReference().child("s");
-        mRef.child("um").setValue(pollDimension);
+//        PollDimension pollDimension = new PollDimension("what", false);
+//        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//        DatabaseReference mRef = firebaseDatabase.getReference().child("s");
+//        mRef.child("um").setValue(pollDimension);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
