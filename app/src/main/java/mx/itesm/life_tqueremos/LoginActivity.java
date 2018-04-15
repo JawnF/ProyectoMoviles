cb048ec0e511e238693dd09380a85d717572f26c
Iván Muñiz <ivanmr_96@live.com.mx>
2018-04-15 18:45:50 -0500

    adds user registration and login verification with firebase

--- app/src/main/java/mx/itesm/life_tqueremos/LoginActivity.java
+++ app/src/main/java/mx/itesm/life_tqueremos/LoginActivity.java
@@ -1,12 +1,21 @@
 package mx.itesm.life_tqueremos;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
@@ -16,17 +25,24 @@ public class LoginActivity extends AppCompatActivity {
     private TextView tvRegistrar;
     private TextInputEditText tietMail;
     private TextInputEditText tietPassword;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupUIViews();
 
-        tietMail = (TextInputEditText) findViewById(R.id.editText_mail);
-        tietPassword = (TextInputEditText) findViewById(R.id.editText_password);
-        btnAcceder = (Button) findViewById(R.id.button_acceder);
-        tvRegistrar = (TextView) findViewById(R.id.textView_registrar);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        progressDialog = new ProgressDialog(this);
+
+//        if(user != null) {
+//            finish();
+//            startActivity(new Intent(this, MenuActivity.class));
+//        }
 
         tvRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,9 +58,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (validar()) {
-                    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                    startActivity(intent);
-                    finish();
+                    validate(tietMail.getText().toString(), tietPassword.getText().toString());
                 }
             }
         });
@@ -69,5 +83,32 @@ public class LoginActivity extends AppCompatActivity {
             tietPassword.setError(null);
         }
         return bValid;
+    }
+
+    private void validate(String userName, String userPassword) {
+        progressDialog.setMessage("Loging in..");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+                    finish();
+                }
+                else {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void setupUIViews() {
+        tietMail = (TextInputEditText) findViewById(R.id.editText_mail);
+        tietPassword = (TextInputEditText) findViewById(R.id.editText_password);
+        btnAcceder = (Button) findViewById(R.id.button_acceder);
+        tvRegistrar = (TextView) findViewById(R.id.textView_registrar);
     }
 }
--- app/src/main/java/mx/itesm/life_tqueremos/SignupActivity.java
+++ app/src/main/java/mx/itesm/life_tqueremos/SignupActivity.java
@@ -1,22 +1,37 @@
 package mx.itesm.life_tqueremos;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
     private TextView etYaRegistrado;
+    private TextInputEditText tietNombre;
+    private TextInputEditText tietEmail;
+    private TextInputEditText tietPassword;
+    private Button btnRegistrar;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        setupUIViews();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        etYaRegistrado = (TextView) findViewById(R.id.editText_yaregistrado);
         etYaRegistrado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,11 +39,57 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validate()) {
+                    //upload data to the database
+                    String email = tietEmail.getText().toString().trim();
+                    String password = tietPassword.getText().toString().trim();
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignupActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                onBackPressed();
+                            }
+                            else {
+                                Toast.makeText(SignupActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    private void setupUIViews() {
+        etYaRegistrado = (TextView) findViewById(R.id.editText_yaregistrado);
+        tietNombre = (TextInputEditText) findViewById(R.id.editText_nombreRegistro);
+        tietEmail = (TextInputEditText) findViewById(R.id.editText_correoRegistro);
+        tietPassword = (TextInputEditText) findViewById(R.id.editText_passwordRegistro);
+        btnRegistrar = (Button) findViewById(R.id.button_registrar);
+    }
+
+    private Boolean validate() {
+        Boolean result = false;
+        String name =  tietNombre.getText().toString();
+        String password = tietPassword.getText().toString();
+        String email = tietEmail.getText().toString();
+
+        if (name.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this,"Please enter all the details", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            result = true;
+        }
+        return result;
     }
 
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
         finish();
