bd4a56fed0a12a11e656ec79716f23499a102ea5
Iván Muñiz <ivanmr_96@live.com.mx>
2018-05-05 16:11:39 -0500

    agrega notificaciones desde firebase

---
+++ app/src/main/java/mx/itesm/life_tqueremos/MyFirebaseInstanceIDService.java
@@ -0,0 +1,19 @@
+package mx.itesm.life_tqueremos;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+//    public static final String TAG = "NOTICIAS";
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+//        String token = FirebaseInstanceId.getInstance().getToken();
+//        Log.d(TAG, "Token: " + token);
+    }
+}
